bc2108746c615ad29a311525e4d087516acab5e9
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-06-20 23:30:41 +0500

    add

--- pizza/src/companents/BackgroundBlack.jsx
+++ pizza/src/companents/BackgroundBlack.jsx
@@ -2,11 +2,6 @@ import React, { useState } from 'react'
 
 export default function BackgroundBlack({bgBlack, CloseBg , setBgBlack}) {
 
-    function CloseBg(){
-        setKarzinkaBox('none')
-        setBgBlack('none')
-      }
-  
     return (
     <div onClick={() => CloseBg()} className={bgBlack}>
 
