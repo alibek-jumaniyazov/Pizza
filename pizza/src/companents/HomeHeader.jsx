b3ac860bbbf46bc64db86ed2f7f2726ae18c6961
Satipov Akbar <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-06-27 17:35:31 +0500

    pusgh

--- pizza/src/companents/HomeHeader.jsx
+++ pizza/src/companents/HomeHeader.jsx
@@ -37,6 +37,7 @@ export default function HomeHeader() {
     function openKarzinka(){
         setKarzinkaBox('karzinkaBlock') 
         setBgBlack('bgBlack')
+        
     }
     return (
         <>
